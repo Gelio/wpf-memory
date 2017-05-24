c433dbcea26537816fd5737600d9c4eaea4f54f7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-05-24 19:29:35 +0200

    complete game logic

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -25,7 +26,9 @@ namespace Memory
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private ObservableCollection<MemoryCard> _memoryCards = new ObservableCollection<MemoryCard>();
-        private MemoryCard _firstCard = null;
+        private MemoryCard _firstCard;
+        private bool _gameStarted;
+        private int _timeLeft = 20;
 
         public ObservableCollection<MemoryCard> MemoryCards
         {
@@ -49,14 +52,73 @@ namespace Memory
             }
         }
 
+        public bool GameStarted
+        {
+            get { return _gameStarted; }
+            set
+            {
+                if (value == _gameStarted) return;
+                _gameStarted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int TimeLeft
+        {
+            get { return _timeLeft; }
+            set
+            {
+                if (value == _timeLeft) return;
+                _timeLeft = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Timer gameTimer = new Timer(1000);
+        private int _cardsGuessed;
+
+        public int CardsGuessed
+        {
+            get { return _cardsGuessed; }
+            set
+            {
+                if (value == _cardsGuessed) return;
+                _cardsGuessed = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             generateCards();
+            gameTimer.Elapsed += (sender, args) =>
+            {
+                TimeLeft--;
+                if (TimeLeft <= 0)
+                    TimeUp();
+            };
+        }
+
+        private void TimeUp()
+        {
+            gameTimer.Stop();
+            MessageBoxResult result = MessageBox.Show(this, "You lost! Would you like to start again?", "Lost", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                GameStarted = false;
+                generateCards();
+                TimeLeft = 20;
+            }
+            else if (result == MessageBoxResult.No)
+            {
+                Close();
+            }
         }
 
         private void generateCards()
         {
+            CardsGuessed = 0;
             MemoryCards.Clear();
             List<MemoryCard> memoryCards = new List<MemoryCard>(16);
             for (int i = 1; i <= 8; i++)
@@ -70,18 +132,6 @@ namespace Memory
                 MemoryCards.Add(card);
         }
 
-        private void CardOnMouseEnter(object sender, MouseEventArgs e)
-        {
-            Button button = sender as Button;
-            button.Background = new SolidColorBrush(Colors.DarkBlue);
-        }
-
-        private void CardOnMouseLeave(object sender, MouseEventArgs e)
-        {
-            Button button = sender as Button;
-            button.Background = new SolidColorBrush(Colors.AliceBlue);
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
@@ -92,6 +142,9 @@ namespace Memory
 
         private void CardOnClick(object sender, RoutedEventArgs e)
         {
+            if (!GameStarted)
+                return;
+
             Button button = sender as Button;
             MemoryCard card = button.DataContext as MemoryCard;
 
@@ -109,6 +162,10 @@ namespace Memory
                 {
                     FirstCard.Visible = false;
                     card.Visible = false;
+                    CardsGuessed++;
+
+                    if (CardsGuessed == 8)
+                        Win();
                 }
                 else
                 {
@@ -117,6 +174,40 @@ namespace Memory
                 }
                 FirstCard = null;
             }
+        }
+
+        private void Win()
+        {
+            gameTimer.Stop();
+            GameStarted = false;
+            MessageBoxResult result = MessageBox.Show(this, "You won! Would you like to start again?", "Win", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                generateCards();
+                TimeLeft = 20;
+            }
+            else if (result == MessageBoxResult.No)
+            {
+                Close();
+            }
+        }
+
+        private void StartOnClick(object sender, RoutedEventArgs e)
+        {
+            GameStarted = !GameStarted;
+
+            if (GameStarted)
+                gameTimer.Start();
+            else
+                gameTimer.Stop();
+        }
+
+        private void ResetOnClick(object sender, RoutedEventArgs e)
+        {
+            GameStarted = false;
+            generateCards();
+            gameTimer.Stop();
+            TimeLeft = 20;
         }
     }
 }
