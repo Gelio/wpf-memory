55eaebf3d4d10f6e10d7162ebb6b96f6312810de
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-05-24 19:02:21 +0200

    complete layout

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Memory.Annotations;
 
 namespace Memory
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private ObservableCollection<MemoryCard> _memoryCards = new ObservableCollection<MemoryCard>();
+
+        public ObservableCollection<MemoryCard> MemoryCards
+        {
+            get { return _memoryCards; }
+            set
+            {
+                if (Equals(value, _memoryCards)) return;
+                _memoryCards = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            generateCards();
+        }
+
+        private void generateCards()
+        {
+            MemoryCards.Clear();
+            List<MemoryCard> memoryCards = new List<MemoryCard>(16);
+            for (int i = 1; i <= 8; i++)
+            {
+                memoryCards.Add(new MemoryCard(i));
+                memoryCards.Add(new MemoryCard(i));
+            }
+
+            memoryCards.Shuffle();
+            foreach (MemoryCard card in memoryCards)
+                MemoryCards.Add(card);
         }
 
         private void CardOnMouseEnter(object sender, MouseEventArgs e)
@@ -35,6 +68,22 @@ namespace Memory
         {
             Button button = sender as Button;
             button.Background = new SolidColorBrush(Colors.AliceBlue);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void CardOnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            MemoryCard card = button.DataContext as MemoryCard;
+
+            card.Selected = !card.Selected;
         }
     }
 }
---
+++ Memory/MemoryCard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Memory.Annotations;
+
+namespace Memory
+{
+    public class MemoryCard : INotifyPropertyChanged
+    {
+        private int _content;
+        private bool _selected;
+
+        public int Content
+        {
+            get { return _content; }
+            set
+            {
+                if (value == _content) return;
+                _content = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Selected
+        {
+            get { return _selected; }
+            set
+            {
+                if (value == _selected) return;
+                _selected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MemoryCard(int content)
+        {
+            Content = content;
+        }
+    }
+}
